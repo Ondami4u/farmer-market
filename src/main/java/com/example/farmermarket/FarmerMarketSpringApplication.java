ca8d8b64a49f012ba5cea03048a8e8973b8f82b9
easyp <roman,yudkevich@gmail.com>
2025-04-24 11:56:37 +0300

    Initial Spring Boot setup

---
+++ src/main/java/com/example/farmermarket/FarmerMarketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerMarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerMarketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/farmermarket/FarmerMarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerMarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
